feecb15af83ea68f41e31795b7bb7654db15c9d4
mlinares <m.linares93@gmail.com>
2025-02-04 08:39:49 -0500

    make setup template

---
+++ src/pages/Setup/Setup.jsx
@@ -0,0 +1,12 @@
+//css
+import styles from "./Setup.module.css" 
+
+const Setup = () => {
+  return ( 
+    <main className={styles.container}>
+      <div>Profile set up</div>
+    </main>
+  );
+}
+
+export default Setup;
