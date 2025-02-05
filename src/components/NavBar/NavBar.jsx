e8704196d545e0acb52db5ad85d6b3a982631a58
mlinares <m.linares93@gmail.com>
2025-02-05 08:26:46 -0500

    update nav bar styling

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -8,18 +8,16 @@ const NavBar = ({ user, handleLogout }) => {
   return (
     <nav className={styles.navBar}>
       <div className={styles.logo}>Folkly</div>
-      {user ?
-        <ul>
-          <li>Welcome, {user.name}</li>
-          <li><NavLink to="/profiles">Profiles</NavLink></li>
-          <li><NavLink to="" onClick={handleLogout}>LOG OUT</NavLink></li>
-          <li><NavLink to="/auth/change-password">Change Password</NavLink></li>
-        </ul>
-      :
-        <ul>
-          <li><NavLink to="/auth/login">Log In</NavLink></li>
-          <li><NavLink to="/auth/signup">Sign Up</NavLink></li>
-        </ul>
+      {user &&
+        <div className={styles.menu}>
+          <i className="fa-solid fa-bars"></i>
+          <div className={styles.userDropDown}>
+            <NavLink to="/profiles">Profiles</NavLink>
+            <NavLink to="" onClick={handleLogout}>LOG OUT</NavLink>
+            <NavLink to="/auth/change-password">Change Password</NavLink>
+          </div>
+        </div>
+
       }
     </nav>
   )
--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,10 +1,27 @@
+// npm modules
+import { NavLink } from 'react-router-dom'
+
 // css
 import styles from './Landing.module.css'
 
 const Landing = ({ user }) => {
   return (
-    <main className={styles.container}>
-      <h1>hello, {user ? user.name : 'friend'}</h1>
+    <main className={styles.container}> 
+    {
+      user ?
+      <div className={styles.pageContent}>
+        <div className={styles.pageTitle}>Welcome {user.name}</div>
+        <div className={styles.chooseCult}>user.</div>
+      </div>
+      :
+      <div className={styles.left}>
+        <div className={styles.logo}>Folkly</div>
+        <div className={styles.logon}>  
+          <NavLink to="/auth/login"><button>Log In</button></NavLink>
+          <NavLink to="/auth/signup"><button>Sign up</button></NavLink>
+        </div>   
+    </div>
+    }
     </main>
   )
 }
