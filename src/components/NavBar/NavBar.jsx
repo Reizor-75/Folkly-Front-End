e93a24aea396fb463a9b7e53e3b6b649048e11c9
mlinares <m.linares93@gmail.com>
2025-02-04 21:07:15 -0500

    update styling for navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,13 @@
 // npm modules
 import { NavLink } from 'react-router-dom'
 
+//css
+import styles from "./NavBar.module.css"
+
 const NavBar = ({ user, handleLogout }) => {
   return (
-    <nav>
+    <nav className={styles.navBar}>
+      <div className={styles.logo}>Folkly</div>
       {user ?
         <ul>
           <li>Welcome, {user.name}</li>
